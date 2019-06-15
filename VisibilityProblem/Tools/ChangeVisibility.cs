d65a01cec06e347fb1e0326f8a91b472c0703dad
SLawpie <slawpie@hotmail.com>
2019-06-15 14:42:46 +0200

    Dodaj pliki projektów.

---
+++ VisibilityProblem/Tools/ChangeVisibility.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using VisibilityProblem.ViewModels;
+
+namespace VisibilityProblem.Tools
+{
+    class ChangeVisibility
+    {
+        public void Change()
+        {
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window.GetType() == typeof(MainWindow))
+                {
+                    (window as MainWindow).TargetButton.Visibility = Visibility.Visible;
+
+                }
+            }
+
+        }
+
+
+        public void Change10Times()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+
+                Application.Current.Dispatcher.Invoke( delegate
+                    {
+                        foreach (Window window in Application.Current.Windows)
+                        {
+                            if (window.GetType() == typeof(MainWindow))
+                            {
+                                (window as MainWindow).TargetButton.Visibility = Visibility.Visible;
+                            }
+                        }
+                    }
+                 );
+
+                Thread.Sleep(1000);
+
+                Application.Current.Dispatcher.Invoke( delegate
+                    {
+                        foreach (Window window in Application.Current.Windows)
+                        {
+                            if (window.GetType() == typeof(MainWindow))
+                            {
+                                (window as MainWindow).TargetButton.Visibility = Visibility.Hidden;
+                            }
+
+                        }
+                    }
+                );
+
+            };
+        }
+
+    }
+}
---
+++ VisibilityProblem/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using VisibilityProblem.Tools;
+
+
+namespace VisibilityProblem.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private Visibility targetButtonVisibility;
+
+        public MainWindowViewModel()
+        {
+            TargetButtonVisibility = Visibility.Hidden;
+        }
+        
+
+
+        public Visibility TargetButtonVisibility
+        {
+            get
+            {
+                return targetButtonVisibility;
+            }
+            set
+            {
+                targetButtonVisibility = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public ICommand MainButtonClick
+        {
+            get
+            {
+                return new RelayCommand(MainButtonExecute);
+            }
+        }
+
+        private void MainButtonExecute()
+        {
+            ChangeVisibility();
+        }
+
+        public void ChangeVisibility()
+        {
+            TargetButtonVisibility = (TargetButtonVisibility == Visibility.Visible) ? TargetButtonVisibility = Visibility.Hidden : TargetButtonVisibility = Visibility.Visible;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
